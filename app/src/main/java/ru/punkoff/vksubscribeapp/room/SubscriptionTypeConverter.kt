012dabeac765de1313ff086efc5b4258cc11555f
Punk_Off <pankov.kirill1997@yandex.ru>
2022-03-31 13:07:06 +0300

    remove GSON lib, use native JSONObject

--- app/src/main/java/ru/punkoff/vksubscribeapp/room/SubscriptionTypeConverter.kt
+++ app/src/main/java/ru/punkoff/vksubscribeapp/room/SubscriptionTypeConverter.kt
@@ -1,14 +1,21 @@
 package ru.punkoff.vksubscribeapp.room
 
 import androidx.room.TypeConverter
-import com.google.gson.Gson
 import com.vk.dto.common.id.UserId
+import org.json.JSONObject
 
 class SubscriptionTypeConverter {
     @TypeConverter
-    fun subscriptionToJson(subscription: UserId?): String = Gson().toJson(subscription)
+    fun subscriptionToJson(subscription: UserId?): String {
+        val jsonObject = JSONObject()
+        jsonObject.put("value", subscription?.value)
+        return jsonObject.toString()
+    }
 
     @TypeConverter
-    fun jsonToSubscription(subscription: String): UserId? =
-        Gson().fromJson(subscription, UserId::class.java)
+    fun jsonToSubscription(subscription: String): UserId {
+        val jsonObject = JSONObject(subscription)
+        val value = jsonObject.getLong("value")
+        return UserId(value)
+    }
 }
